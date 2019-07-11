d8e613fdd4859504427d417202616febc51c04c9
lophas <lophas@lophass-Mini.lan>
2019-07-11 11:38:25 +0200

    check if Jetpack connection live

--- jetpack-view-counter.php
+++ jetpack-view-counter.php
@@ -4,7 +4,7 @@
  * Plugin URI: https://github.com/lophas/jetpack-view-counter
  * GitHub Plugin URI: https://github.com/lophas/jetpack-view-counter
  * Description:
- * Version: 2.2
+ * Version: 2.3
  * Author: Attila Seres
  * Author URI:
  * License: GPLv2
@@ -17,8 +17,6 @@ class Jetpack_View_Counter
     const HOOK = __CLASS__;
   	const CACHE_HOURS = 1;
     private $schedule;
-//    const META_KEY = '_jetpack_post_views_count';//apply_filters( 'view_counter_meta_key', self::META_KEY )
-
     private static $_instance;
     public function instance()
     {
@@ -30,46 +28,38 @@ class Jetpack_View_Counter
     public function __construct()
     {
         add_action('plugins_loaded', function () {
-            if (!defined('JETPACK__VERSION')) {
-                return;
-            }
+            if (!defined('JETPACK__VERSION')) return;
             add_action('init', array( $this, 'init' ));
-            if (is_admin()) {
-                add_action('admin_init', [$this, 'admin_init']);
-            }
         });
     } //__construct
-
     public function init()
     {
+        if ( ! class_exists( 'Jetpack' ) || ! Jetpack::is_active()) return;
         // Settings page
-        add_action('admin_init', array( $this, 'settings_page_init' ));
-        add_action('admin_menu', array( $this, 'add_settings_page' ));
-        add_filter('plugin_action_links', array( $this, 'add_settings_link' ), 10, 2);
-
+        if (is_admin()) {
+            add_action('admin_init', [$this, 'admin_init']);
+            add_action('admin_init', array( $this, 'settings_page_init' ));
+            add_action('admin_menu', array( $this, 'add_settings_page' ));
+            add_filter('plugin_action_links', array( $this, 'add_settings_link' ), 10, 2);
+        }
         // Shortcode
         add_shortcode('view-count', array( $this, 'view_count_shortcode' ));
-
         add_filter('cron_schedules', function($schedules){
            if(!isset($schedules[$this->schedule])) $schedules[$this->schedule] = array( 'interval' => intval($this->schedule) * 60, 'display' => 'Every '.$this->schedule);
            return $schedules;
          });
      		add_action(self::HOOK, array($this, 'get_views'));
-
         $this->schedule = absint(apply_filters('view_counter_expiration', self::CACHE_HOURS) * 60); //minutes
         if($this->schedule < 5) $this->schedule = 5; //Jetpack cache time
         $this->schedule .= ' minutes';
         if($cron = wp_get_scheduled_event( self::HOOK )) if($cron->schedule != $this->schedule) wp_clear_scheduled_hook(self::HOOK);
         if (!wp_next_scheduled ( self::HOOK )) wp_schedule_event(time(), $this->schedule, self::HOOK);
-
         $settings = $this->get_option();
         foreach((array)$settings['post_types'] as $post_type) if(!get_transient(__CLASS__.'_'.$post_type)) {
           $this->get_views($settings);
           break;
         }
-//        add_filter( "get_post_metadata", function($meta_value, $post_id, $meta_key, $single ) {return $meta_key == $this->get_meta_key() ? $this->get_view_count($post_id) : $meta_value;}, PHP_INT_MAX, 4);
     }
-
     public function get_views($settings = false) {
       if(!$settings) $settings = $this->get_option();
       foreach((array)$settings['post_types'] as $post_type) {
@@ -90,7 +80,6 @@ class Jetpack_View_Counter
     /**
      * Save WordPress.com views as post meta data
      */
-
     /**
      * Initialize plugin options
      */
@@ -98,7 +87,6 @@ class Jetpack_View_Counter
     {
         register_setting('view_counter_options', self::OPTIONS, array( $this, 'view_counter_validate' ));
     }
-
     /**
      * Add Settings Page
      *
@@ -107,7 +95,6 @@ class Jetpack_View_Counter
     {
         add_options_page(__('View Counter Settings', 'view-counter'), __('View Counter', 'view-counter'), 'manage_options', 'wp_stats_view_counter', array( $this, 'settings_page' ));
     }
-
     /**
      * Build Settings Page
      *
@@ -134,7 +121,6 @@ class Jetpack_View_Counter
                             $checked = checked(in_array($cpt->name, $post_types) ? true : false, true, false);
                             $name = esc_attr($cpt->name);
                             $label = esc_html($cpt->label);
-
                             printf('<label><input type="checkbox" %s name="'.self::OPTIONS.'[post_types][]" value="%s" /> %s </label><br />', $checked, $name, $label);
                         } ?>
 						</td>
@@ -147,7 +133,6 @@ class Jetpack_View_Counter
 		</div>
 		<?php
     }
-
     /**
      * Add Settings Link
      *
@@ -155,24 +140,18 @@ class Jetpack_View_Counter
     public function add_settings_link($links, $file)
     {
         static $this_plugin;
-
         if (empty($this_plugin)) {
             $this_plugin = plugin_basename(__FILE__);
         }
-
         // Check to make sure we're on the right plugin
         if ($file == $this_plugin) {
-
             // Create link
             $settings_link = '<a href="' . admin_url('options-general.php?page=wp_stats_view_counter') . '">' . __('Settings', 'view-counter') . '</a>';
-
             // Add link to list
             array_unshift($links, $settings_link);
         }
-
         return $links;
     }
-
     /**
      * Validate settings
      *
@@ -182,7 +161,6 @@ class Jetpack_View_Counter
         $this->get_views($settings);
         return $settings;
     }
-
     /**
      * View count shortcode
      *
@@ -198,30 +176,23 @@ class Jetpack_View_Counter
         if (get_post_status() != 'publish') {
             return;
         }
-
         $defaults = array(
             'after'  => '',
             'before' => '',
         );
         $atts = shortcode_atts($defaults, $atts);
-
-        //		$views = number_format_i18n( (double) get_post_meta( $post->ID, $this->get_meta_key(), true ) );
         $views = number_format_i18n((double) $this->get_view_count());
-
         if ($views) {
             $output = sprintf('<span class="view-count">%2$s%1$s%3$s</span>', $views, $atts['before'], $atts['after']);
         }
-
         return $output;
     }
-
     public function admin_init()
     {
         $settings = $this->get_option();
         if (!in_array($GLOBALS['typenow'], $settings['post_types'])) {
             return;
         }
-
         add_action('admin_head-edit.php', function () {
             ?>
 <style>
@@ -232,8 +203,6 @@ class Jetpack_View_Counter
 </style>
 <?php
         });
-
-
         add_filter('manage_edit-'.$GLOBALS['typenow'].'_sortable_columns', function ($columns) {
             $columns['pageviews'] = array('post_views',1);
             return $columns;
@@ -248,17 +217,14 @@ class Jetpack_View_Counter
             if (get_post_status($post_id) != 'publish') {
                 return;
             }
-
             if ('pageviews' !== $colname) {
                 return false;
             }
             $view_count = $this->get_view_count($post_id);
-//            $view_count = get_post_meta($post_id, $this->get_meta_key(), true);
             // Print Jetpack post views
             if ($view_count) {
                 echo  number_format(absint($view_count)) ;
             }
-            //if(is_super_admin()) echo ' ['.date('H:i', apply_filters( 'view_counter_expiration', self::CACHE_HOURS )*HOUR_IN_SECONDS - time() + get_post_meta($post_id, $this->get_meta_key().'_created', true)).']';
         }, 10, 2);
             add_action('load-edit.php', function(){
               add_action('pre_get_posts', [$this, 'pre_get_posts']);
@@ -284,13 +250,11 @@ class Jetpack_View_Counter
         }
         if($views = get_transient(__CLASS__.'_'.get_post_type($post_id))) return $views[$post_id];
     }
-
     public function get_option()
     {
         $options = get_option(self::OPTIONS, ['post_types' => ['post']]);
         return $options;
     }
 } //class
-
 Jetpack_View_Counter::instance();
 endif;
